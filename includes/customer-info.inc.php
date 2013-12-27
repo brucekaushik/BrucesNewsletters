b5ebd36213b450b9d5bd8a1e71f954e8188257c5
Kaushik <kaushik.srinivas@gmail.com>
2013-12-27 23:38:11 +0530

    done some variable handling

--- includes/customer-info.inc.php
+++ includes/customer-info.inc.php
@@ -1,14 +1,21 @@
 <?php
 
+// connect with database
 require '../BrucesAdminArea/includes/dbConnect.inc.php';
 
+// handle variables
+$ses_user_id = $_SESSION['user_id'];
+
+// query the database for details of subcribers
 $userDetailsQuery = "SELECT * FROM NlCustomers";
 $userDetailsRes = mysql_query($userDetailsQuery, $c);
 
+// populate above details into an array (for convenience)
 while($userDetailsRow = mysql_fetch_array($userDetailsRes, MYSQL_ASSOC)){
 	$customer_info[] = $userDetailsRow;
 }
 
+// query the database for the current user details (who has logged in)
 $custQuery = "SELECT * FROM NlCustomers WHERE cust_id='$ses_user_id'";
 $custRes = mysql_query($custQuery, $c);
 $custDet = mysql_fetch_array($custRes, MYSQL_ASSOC);
