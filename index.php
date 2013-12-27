82e892c3e680db75366e357b13d1edf91071796d
Kaushik <kaushik.srinivas@gmail.com>
2013-12-27 19:01:02 +0530

    authenticate with BrucesAdminArea and redirect back

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+session_start();
+
+$_SESSION['verify_back_to'] = "../BrucesNewsletters/home.php";
+$_SESSION['app_name'] = "Bruce's Newsletters";
+
 // redirect to home page
-header ("Location: ../BrucesAdminArea/index.php");
+header ("Location: ../BrucesAdminArea/index.php?action=VerifyLogin");
 
 ?>
