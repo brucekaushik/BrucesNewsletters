ac54b8964307981d97376112f17bd0f1ea582147
Kaushik <kaushik.srinivas@gmail.com>
2013-12-27 23:38:42 +0530

    done some variable handling, added some useful comments

--- manage-subscription.php
+++ manage-subscription.php
@@ -1,5 +1,7 @@
 <?php
 
+// TODO: any person can get to this page directly, change this later..
+
 // start a session
 session_start();
 
@@ -9,8 +11,13 @@ print_r($_SESSION);
 echo "</pre>";
 //*/
 
+// connect to the database
 require '../BrucesAdminArea/includes/dbConnect.inc.php';
+
+// include the customer details array
 require 'includes/customer-info.inc.php';
+
+// include server side validation functions
 require 'includes/serverSideValidation.inc.php';
 
 ?>
@@ -34,13 +41,19 @@ require 'includes/serverSideValidation.inc.php';
 		
 			<?php 
 			
+			// check if the user has submitted the subscription-form
 			if(isset($_POST["subscription-req"])){
 				
+				// store customer email id, validate
 				$cust_email = $_POST['email_id'];
 				$cust_email = validate_email($cust_email);
 				
+				// echo count($custDet);
+				// if count=1 the customer does not exists and can be added as new customer
+				// verify the same
 				if(count($custDet) == 1){
 					
+					// check if email id passed validation
 					if($cust_email != "bad input"){
 						
 						$insertQuery = "INSERT INTO NlCustomers SET cust_id='$ses_user_id',cust_email='$cust_email',sub_status='yes'";
@@ -53,7 +66,9 @@ require 'includes/serverSideValidation.inc.php';
 							echo "<div class='heading red'>There has been some error in your submission, please try again later!";
 						}
 							
-					}else{
+					}
+					// show the bad input form
+					else{
 						echo "<div class='heading red'>Bad Input</div>";
 						echo "<div class='heading'>Subscribe to our Newsletter</div>";
 						echo "<form name='subscription-form' action='manage-subscription.php' method='post'>";
@@ -63,11 +78,14 @@ require 'includes/serverSideValidation.inc.php';
 						echo "</form>";
 					}
 					
+				}
+				// the customer is already existing, and attempting to change the subscription details  
+				else{
 					
-				}else{
-					
+					// check if the user wants to subscribe
 					if ($_POST["subscription-req"] == "subscribe") {
 						
+						// if email id passed validation
 						if($cust_email != "bad input"){
 							
 							$insertQuery = "UPDATE NlCustomers SET cust_email='$cust_email',sub_status='yes' where cust_id='$ses_user_id'";
@@ -80,7 +98,9 @@ require 'includes/serverSideValidation.inc.php';
 								echo "<div class='heading red'>There has been some error in your submission, please try again later!";
 							}
 							
-						}else{
+						}
+						// show bad input form
+						else{
 							echo "<div class='heading red'>Bad Input</div>";
 							echo "<div class='heading'>Subscribe Again To Our Newsletter</div>";
 							echo "<form name='re-subscription-form' action='manage-subscription.php' method='post'>";
@@ -90,8 +110,11 @@ require 'includes/serverSideValidation.inc.php';
 							echo "</form>";
 						}
 						
-					}else if ($_POST["subscription-req"] == "edit-email") {
+					}
+					// check if user wants to edit subscribed email
+					else if ($_POST["subscription-req"] == "edit-email") {
 						
+						// if email id passed validation
 						if($cust_email != "bad input"){
 							
 							$updateQuery = "UPDATE NlCustomers SET cust_email='$cust_email' WHERE cust_id='$ses_user_id'";
@@ -104,7 +127,9 @@ require 'includes/serverSideValidation.inc.php';
 								echo "<div class='heading red'>There has been some error in your submission, please try again later!";
 							}
 							
-						}else{
+						}
+						// show bad input form
+						else{
 							echo "<div class='heading red'>Bad Input</div>";
 							echo "<div class='heading'>Edit Your Email Id</div>";
 							echo "<form name='edit-subscription-form' action='manage-subscription.php' method='post'>";
@@ -114,8 +139,11 @@ require 'includes/serverSideValidation.inc.php';
 							echo "</form>";
 						}
 						
-					}else if($_POST["subscription-req"] == "unsubscribe"){
+					}
+					// check if user wants to unsubscribe
+					else if($_POST["subscription-req"] == "unsubscribe"){
 						
+						// if email id passed validation
 						if($cust_email != "bad input"){
 			
 							$updateQuery = "UPDATE NlCustomers SET sub_status='no' WHERE cust_id='$ses_user_id'";
@@ -128,7 +156,9 @@ require 'includes/serverSideValidation.inc.php';
 								echo "<div class='heading red'>There has been some error in your submission, please try again later!";
 							}	
 							
-						}else{
+						}
+						// show bad input form
+						else{
 							echo "<div class='red'>Bad Input</div>";
 							echo "<div class='heading'>Unsubscribe from our Newsletter</div>";
 							echo "<form name='unsubscription-form' action='manage-subscription.php' method='post'>";
@@ -145,7 +175,8 @@ require 'includes/serverSideValidation.inc.php';
 			}else{
 				
 				// echo count($custDet);
-				
+				// if count=1 then user is not subscribed and also is not a previously unsubscribed customer 
+				// verify the same
 				if(count($custDet) == 1){
 					echo "<form name='subscription-form' action='manage-subscription.php' method='post'>";
 					echo "<div class='heading'>Subscribe to our Newsletter</div>";
@@ -154,6 +185,8 @@ require 'includes/serverSideValidation.inc.php';
 					echo "<div><input type='submit' value='subscribe'></div>";
 					echo "</form>";
 				}else{
+					
+					// if the customer is curretly unsubscribed
 					if($custDet["sub_status"] == "no"){
 						echo "<form name='re-subscription-form' action='manage-subscription.php' method='post'>";
 						echo "<div class='heading'>Subscribe Again To Our Newsletter</div>";
@@ -161,7 +194,9 @@ require 'includes/serverSideValidation.inc.php';
 						echo "<div>Enter Your Email <input name='email_id' type='text' value=''></div>";
 						echo "<div><input type='submit' value='save changes'></div>";
 						echo "</form>";
-					}else{
+					}
+					// if the customer is currently subscribed
+					else{
 						echo "<form name='edit-subscription-form' action='manage-subscription.php' method='post'>";
 						echo "<div class='heading'>Edit Your Email Id</div>";
 						echo "<input type='hidden' name='subscription-req' value='edit-email'>";
