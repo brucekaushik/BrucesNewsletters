1d4ef88893e4ea9f255aec535b2a2bf294eb7c29
Kaushik <kaushik.srinivas@gmail.com>
2013-12-27 19:38:59 +0530

    some variable handling done

--- index2.php
+++ index2.php
@@ -3,22 +3,34 @@
 // start a session
 session_start();
 
+/*
 echo "<pre>";
 print_r($_SESSION);
 echo "</pre>";
+//*/
 
+// handle variables
+$ses_username = $_SESSION['username'];
+unset($_SESSION['app_name']);
+unset($_SESSION['verify_back_to']);
+unset($_SESSION['action']);
+
+// connect to database
 require '../BrucesAdminArea/includes/dbConnect.inc.php';
 
+// query the database for user details
 $userDetailsQuery = "select * from AdminArea where username='$ses_username'";
 $userDetailsRes = mysql_query($userDetailsQuery, $c);
 $userDetailsRow = mysql_fetch_array($userDetailsRes, MYSQL_ASSOC);
 
+/*
 echo "<pre>";
 print_r($userDetailsRow);
 echo "</pre>";
+//*/
 
-$ses_user_id = $userDetailsRow["user_id"];
-session_register("ses_user_id");
+// set session variables
+$_SESSION['user_id'] = $userDetailsRow["user_id"];
 
 // redirect to home page
 header ("Location: home.php");
