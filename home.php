cff5a3074d16ba3c1740f0c7a5143094d824c841
Kaushik <kaushik.srinivas@gmail.com>
2013-12-27 23:40:25 +0530

    done some variable handling

--- home.php
+++ home.php
@@ -9,6 +9,10 @@ print_r($_SESSION);
 echo "</pre>";
 //*/
 
+// handle variables
+$ses_user_level = $_SESSION['user_level'];
+
+// show relavant page depending on level of user
 if($ses_user_level == "reg"){
 	header ("Location: manage-subscription.php");
 }elseif ($ses_user_level == "admin" || $ses_user_level == "root"){
